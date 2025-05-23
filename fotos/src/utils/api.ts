d314a191ccca4c33295ec16faac6147b071322fa
chavarahul <chavarahul7@gmail.com>
2025-05-24 04:06:20 +0530

    changed production url

--- fotos/src/utils/api.ts
+++ fotos/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://backend-google-three.vercel.app",
 });
 
 axiosInstance.interceptors.request.use((config) => {
